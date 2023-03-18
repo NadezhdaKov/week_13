359ea62f2388f876b7ae8bd6dccd5b1587a99c91
NadezhdaKov <anita.k.blake@gmail.com>
2023-03-18 23:50:01 +0300

    first cimmit

---
+++ index.js
@@ -0,0 +1,52 @@
+console.log('Вопрос 1. Зарплата вечерего уборщика, имеющего голос и педагогическое образование:');
+let cleaner = 40000; //Зарплата вечернего уборщика в городской поликлинике
+let vocalTeacher = 22000; //Зарплата педагога по вокалу
+console.log(cleaner + vocalTeacher);
+
+console.log("\n");
+console.log('Вопрос 2. Название песни.');
+console.log('Владивосток' + ' 2000');
+
+console.log("\n");
+console.log('Вопрос 3. Прокрастинирующая Русалка.');
+let 
+    messenger = 128,
+    photo = 254,
+    article = 137,
+    YouTube = 201;
+console.log(`Итого прокрастинация Русалки составила ${(messenger + photo + article + YouTube)/60} часов`);
+
+console.log("\n");
+console.log('Вопрос 4. Число 2 в степени 5.');
+console.log(2 ** 5);
+
+console.log("\n");
+console.log('Вопрос 5. Строка.');
+console.log('"Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"')
+
+console.log("\n");
+console.log('Вопрос 6.');
+const greetings = "Привет";
+const name = "Кот";
+console.log(greetings + ', ' + name);
+
+console.log("\n");
+console.log('Вопрос 7. Время в пути');
+let time;
+time = 34;
+console.log(`Старт поездки. Осталось минут: ` + time);
+time -= 15;
+console.log('Немного сторис в соцсетях. Осталось минут: ' + time);
+time -=10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log(`"Вы приехали. Добро пожаловать в Москву"`);
+
+console.log("\n");
+console.log('Вопрос 8. Градусы');
+let fahrenheit = 451;
+let celsius;
+celsius = ((fahrenheit-32) / 1.8);
+console.log(`${fahrenheit} градуса по Фаренгейту — это ${celsius} градуса по Цельсию.`);
+
+
